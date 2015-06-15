8ce40f52be579ab6bc1558c2745c611d17364a20
galler-alexander <galler.aleksandr@gmail.com>
2015-06-15 20:27:36 +0700

    Add Git

--- base/scripts/lib/build_app.sh
+++ base/scripts/lib/build_app.sh
@@ -5,7 +5,15 @@ BUNDLE_DIR=/tmp/bundle-dir
 
 # sometimes, directly copied folder cause some wierd issues
 # this fixes that
+if [[ $GIT_REPO_URL ]]; then
+  if [[ $BRANCH_NAME ]]; then
+    git clone $GIT_REPO_URL -b $BRANCH_NAME /app
+  else
+    git clone $GIT_REPO_URL /app
+  fi
+fi
 cp -R /app $COPIED_APP_PATH
+mv $COPIED_APP_PATH/.ssh ~
 cd $COPIED_APP_PATH
 
 meteor build --directory $BUNDLE_DIR --server=http://localhost:3000
@@ -16,6 +24,7 @@ npm i
 mv $BUNDLE_DIR/bundle /built_app
 
 # cleanup
+rm -rf /app
 rm -rf $COPIED_APP_PATH
 rm -rf $BUNDLE_DIR
 rm -rf ~/.meteor
